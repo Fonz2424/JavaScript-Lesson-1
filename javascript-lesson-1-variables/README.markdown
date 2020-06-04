31e2e929dcec27309e95a77885ffde290375b352
Fonz2424 <65732402+Fonz2424@users.noreply.github.com>
2020-06-04 14:59:58 -0400

    JavaScript-Lesson-1-Variables

---
+++ javascript-lesson-1-variables/README.markdown
@@ -0,0 +1,5 @@
+# JavaScript-Lesson 1-Variables
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Fonz2424/pen/zYvQBMG](https://codepen.io/Fonz2424/pen/zYvQBMG).
+
+
---
+++ javascript-lesson-1-variables/dist/script.js
@@ -0,0 +1,14 @@
+var firstName = "Alfonzo"; //store string Alfonzo in variable firstName
+console.log(firstName); // expected output "Alfonzo"
+var lastName = "Williams";// store string Williams in variable lastName
+console.log(lastName);// expected output: "Williams"
+var fullName = `${firstName}  ${lastName}`; // store firstName and lastName in variable fullName
+console.log(fullName);// expected output "Alfonzo Williams"
+
+
+
+var test = 5; 
+var test1 = "5";
+
+console.log(typeof(test));
+console.log(typeof(test1));
